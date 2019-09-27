bc719da9601cb8f1be9473cd7f32443248f843db
陈永鑫 <1453974550@qq.com>
2019-09-27 15:39:44 +0800

    first test

---
+++ src/main/java/person/cyx/community/CommunityApplication.java
@@ -0,0 +1,18 @@
+package person.cyx.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CommunityApplication
+ * @author chenyongxin
+ * @date 2019/09/27
+ * */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
